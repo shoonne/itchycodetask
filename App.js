1ede91511315ae61ebff28dbb319ca2fc76ce2a0
Shaon Ahmed <shaon_ahmed@msn.com>
2020-03-24 20:47:49 +0100

    Extraced logic from return

--- App.js
+++ App.js
@@ -1,35 +1,42 @@
 import React, { useState, useEffect } from 'react';
-import { View, Animated } from 'react-native';
+import { View } from 'react-native';
 import Question from './src/components/Question';
 import QuestionCounter from './src/components/QuestionCounter';
-import color from './constants/Colors';
-import questionsData from './constants/QuestionsData';
 import Button from './src/components/Button';
 import Header from './src/components/Header';
 import Overlay from './src/components/Overlay';
+import color from './constants/Colors';
+import questionsData from './constants/QuestionsData';
 
-function convertData(arr) {
-  // Convert data to desired format. 
-  var result = arr.reduce(function(res, current) {
-    return Object.assign(res, current);
-  }, {}, 0);
-
-  return result;
-};
 
 
 const App = () => {
-
   const [questionNumber, setQuestionNumber] = useState(0); // This is for controlling the active question
   const [data, setData] = useState([]);   // This is for collection the data from questions
   const [result, setResult] = useState()  // This is used to store the desired format ;
   //const [showFollowUp, setShowFollUp] = useState(false)
+
   let isMax = questionNumber === 10;
 
+  const btnPress = (point) => {
+    setQuestionNumber(questionNumber + 1); // Go to next question
+    setData([
+      ...data, 
+      {[questionsData[questionNumber].name]: point
+      }]); // Store data.
+  }
+
+  const convertData = (arr) => {
+    // Convert data to desired format. 
+    var result = arr.reduce(function(res, current) {
+      return Object.assign(res, current);
+    }, {}, 0);
+  
+    return result;
+  };
 
 
-
-  // If we have reached the 10th question, 
+  // Once we have answered 10th question, 
   // reset the states and convert and store the result 
   if(data && questionNumber === 10) {
     setQuestionNumber(0)
@@ -39,20 +46,19 @@ const App = () => {
 
 
   useEffect(()=> {
-    if(result) {
-      console.log(result)
+    if(result && questionNumber === 0) {
+      console.log(result) // Result
     }
   })
 
 
     return (
       <View style={styles.container}>
-      <Overlay questionNumber={questionNumber}/>
-      <Header text="Välmående DLQ!"/>
-      <QuestionCounter questionNumber={questionNumber + 1}/>
-      <Question 
-      question={questionsData[isMax ? 0 : questionNumber].question}
-
+        <Overlay questionNumber={questionNumber}/>
+        <Header text="Välmående DLQ!"/>
+        <QuestionCounter questionNumber={questionNumber + 1}/>
+        <Question 
+        question={questionsData[isMax ? 0 : questionNumber].question}
         />
       
       {  questionNumber === 6 ? 
@@ -68,32 +74,20 @@ const App = () => {
         <View style={{marginBottom: 100, zIndex:2}}>
             <Button
               text={"Väldigt mycket"}
-              onPress={() => {
-                setQuestionNumber(questionNumber + 1);
-                setData([...data, {[questionsData[questionNumber].name]: 3}]);
-                }}
+              onPress={() => btnPress(3)}
               />
 
             <Button
-            text={"En hel del"}
-            onPress={() => {
-              setQuestionNumber(questionNumber + 1);
-              setData([...data, {[questionsData[questionNumber].name]: 2}]);
-              }}
-            />
+              text={"En hel del"}
+              onPress={() => btnPress(2)}
+              />
             <Button
-            text={"Lite grann"}
-            onPress={() => {
-              setQuestionNumber(questionNumber + 1);
-              setData([...data, {[questionsData[questionNumber].name]: 1}]);
-              }}
-            />
+              text={"Lite grann"}
+              onPress={() => btnPress(1)}
+              />
             <Button
             text={"Inte alls"}
-            onPress={() => {
-              setQuestionNumber(questionNumber + 1);
-              setData([...data, {[questionsData[questionNumber].name]: 0}]);
-              }}
+            onPress={() => btnPress(0)}
             />
           </View>}
           </View>
--- src/components/Question.js
+++ src/components/Question.js
@@ -1,4 +1,4 @@
-import React, {Component, useState} from 'react';
+import React, {Component} from 'react';
 import { Text, Animated } from 'react-native';
 import color from '../../constants/Colors';
 
